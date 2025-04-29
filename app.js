638bcbc329773884b886bb8421f97e25a16acc12
Moneyboy.buy <michyqruz@gmail.com>
2025-04-29 10:22:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ fetchWeather();
 // Service Worker Registration
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('sw.js')
       .then(registration => {
         console.log('ServiceWorker registration successful');
       })
