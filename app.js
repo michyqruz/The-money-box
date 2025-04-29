e4f8cd24978c43aea4552dfce91c4fb79ffb1f58
Moneyboy.buy <michyqruz@gmail.com>
2025-04-29 18:49:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ window.addEventListener('pageshow', (event) => {
 // Service Worker Registration
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('sw.js')
             .then(registration => {
                 console.log('ServiceWorker registered');
             })
