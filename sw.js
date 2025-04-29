dd46ed2d6a21b974e0725865eab74de1b57f6ebc
Moneyboy.buy <michyqruz@gmail.com>
2025-04-29 11:29:49 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,72 +1,23 @@
-const CACHE_NAME = 'weather-pwa-v1';
+const CACHE_NAME = 'pwa-only-cache-v1';
 const urlsToCache = [
-  '',
-  'index.html',
-  'styles.css',
-  'app.js',
-  'icon-192x192.png',
-  'icon-512x512.png',
-  'sunny.png',
-  'cloudy.png',
-  'rainy.png'
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        
-        const fetchRequest = event.request.clone();
-        
-        return fetch(fetchRequest).then(
-          response => {
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            
-            const responseToCache = response.clone();
-            
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-              
-            return response;
-          }
-        ).catch(() => {
-          if (event.request.url.includes('.png')) {
-            return caches.match('sunny.png');
-          }
-          return new Response('Offline - Weather data unavailable');
-        });
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .then(response => response || fetch(event.request))
   );
 });
