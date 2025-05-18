efeb0b8a3f84a546d5fbdb1e73064ed21a39f90a
Moneyboy.buy <michyqruz@gmail.com>
2025-05-18 13:10:45 +0100

    Update edit.js

--- edit.js
+++ edit.js
@@ -2,7 +2,7 @@
 function checkNameAndIDInLocalStorage(userDataArray) {
   // Retrieve stored values from local storage
   const storedName = localStorage.getItem('walletName');
-  const storedID = localStorage.getItem('WalletUserld');
+  const storedID = localStorage.getItem('walletUserld');
 
   // Check if both values exist in local storage
   if (storedName && storedID) {
